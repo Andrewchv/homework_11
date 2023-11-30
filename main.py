9b9290625c314d715eabeb52056ed9793cc1c52e
Andrewchv <143516241+Andrewchv@users.noreply.github.com>
2023-11-30 15:24:54 +0100

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from collections import UserDict
 from datetime import datetime
+from shlex import split
 
 
 class Field:
@@ -21,21 +22,44 @@ class Field:
 
 
 class Name(Field):
-    pass
+    def __str__(self):
+        return str(self.value)
 
 
 class Phone(Field):
-    def validate(self, value):
-        if not (isinstance(value, str) and len(value) == 10 and value.isdigit()):
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        self.validate_phone(new_value)
+        self._value = new_value
+
+    def validate_phone(self, value):
+        if value is not None and (not isinstance(value, str) or not (len(value) == 10 and value.isdigit())):
             raise ValueError("Invalid phone number format")
 
 
 class Birthday(Field):
-    def validate(self, value):
-        try:
-            datetime.strptime(value, '%Y-%m-%d')
-        except ValueError:
-            raise ValueError("Invalid birthday format. Please use YYYY-MM-DD")
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        self.validate_birthday(new_value)
+        self._value = datetime.strptime(new_value, '%Y-%m-%d')
+    
+    def validate_birthday(self, value):
+        if value is not None:
+            try:
+                datetime.strptime(value, '%Y-%m-%d')
+            except ValueError:
+                raise ValueError("Invalid birthday format. Please use YYYY-MM-DD")
+    
+    def __str__(self):
+        return str(self.value.strftime('%Y-%m-%d'))
 
 
 class Record:
@@ -50,20 +74,20 @@ class Record:
     def remove_phone(self, phone):
         self.phones = [p for p in self.phones if str(p) != phone]
 
+    def find_phone(self, phone):
+        found_phones = [p.value for p in self.phones if str(p) == phone]
+        return found_phones[0] if found_phones else None
+
     def edit_phone(self, old_phone, new_phone):
         for phone in self.phones:
-            if str(phone) == old_phone:
+            if str(phone.value) == old_phone:
                 phone.value = new_phone
                 break
         else:
             raise ValueError("Phone number does not exist in the record")
 
     def get_phones(self):
-        return [str(phone) for phone in self.phones]
-
-    def find_phone(self, phone):
-        found_phones = [p for p in self.phones if str(p) == phone]
-        return found_phones[0] if found_phones else None
+        return [str(phone.value) for phone in self.phones]
 
     def days_to_birthday(self):
         if self.birthday:
@@ -76,7 +100,9 @@ class Record:
         return None
 
     def __str__(self):
-        return f"Contact name: {self.name}, phones: {', '.join(str(p) for p in self.phones)}, birthday: {self.birthday}"
+        phones_str = ', '.join(str(phone.value) for phone in self.phones)
+        birthday_str = f", birthday: {self.birthday}" if self.birthday else ""
+        return f"Contact name: {self.name}, phones: {phones_str}{birthday_str}"
 
 
 class AddressBook(UserDict):
@@ -86,14 +112,28 @@ class AddressBook(UserDict):
         self.current_page = 1
 
     def add_record(self, record):
-        self.data[record.name.value] = record
+        if record.name.value.lower() in (existing_record.name.value.lower() for existing_record in self.data.values()):
+            existing_record = next(existing_record for existing_record in self.data.values() if existing_record.name.value.lower() == record.name.value.lower())
+            existing_record.phones.extend(record.phones)
+        else:
+            self.data[record.name.value] = record
 
     def find(self, name):
-        return self.data.get(name)
+        name_lower = name.lower()
+        return next((record for record in self.data.values() if record.name.value.lower() == name_lower), None)
 
     def delete(self, name):
-        if name in self.data:
-            del self.data[name]
+        record = next((record for record in self.data.values() if record.name.value.lower() == name.lower()), None)
+        if record:
+            del self.data[record.name.value]
+
+    def change_phone(self, name, old_phone, new_phone):
+        name_lower = name.lower()
+        record = self.find(name_lower)
+        if record:
+            record.edit_phone(old_phone, new_phone)
+        else:
+            raise ValueError(f"Contact {name} not found")
 
     def __iter__(self):
         return self.record_iterator()
@@ -144,31 +184,30 @@ def add_contact(name, phone, birthday=None):
 
 
 @input_error
-def delete_contact(name):
-    contact = contacts.find(name)
-    if contact:
-        contacts.delete(name)
-        return f"Contact {name} deleted"
-    else:
-        return f"Contact {name} not found"
+def delete_contact(command):
+    name = command[1]
+    contacts.delete(name)
+    return f"Contact {name} deleted"
 
 
 @input_error
-def get_phone(name):
+def change_phone(name, new_phone):
     contact = contacts.find(name)
+    if contact:
+        contact.edit_phone(contact.phones[0].value, new_phone)
+        return f"Phone number for {name} changed to {new_phone}"
+    else:
+        raise ValueError(f"Contact {name} not found")
+
+
+@input_error
+def get_phones(command):
+    name = split(command)[1]
+    contact = next((record for record in contacts.data.values() if record.name.value.lower() == name.lower()), None)
     if contact and contact.phones:
-        return f"Phone numbers for {name}: {', '.join(contact.get_phones())}"
+        return f"Phone numbers for {contact.name.value}: {', '.join(map(str, contact.get_phones()))}"
     else:
-        raise ValueError(f"Contact {name} not found")
-
-
-@input_error
-def change_phone(name, old_phone, new_phone):
-    contact = contacts.find(name)
-    if contact:
-        contact.edit_phone(old_phone, new_phone)
-    else:
-        raise ValueError(f"Contact {name} not found")
+        raise ValueError(f"Contact {name.capitalize()} not found")
 
 
 @input_error
@@ -185,6 +224,19 @@ def show_n_records(n):
     return f"{n} records:\n{result}"
 
 
+@input_error
+def days_to_birthday(command):
+    name = command.split()[1]
+    contact = contacts.find(name)
+    if contact and contact.birthday:
+        days_remaining = contact.days_to_birthday()
+        return f"Days to {contact.name.value}'s birthday: {days_remaining} days"
+    elif contact and not contact.birthday:
+        return f"{contact.name.value} doesn't have a birthday set."
+    else:
+        raise ValueError(f"Contact {name} not found")
+
+
 def main():
     while True:
         command = input("Enter command: ").lower()
@@ -192,24 +244,23 @@ def main():
         if command == "hello":
             print("How can I help you?")
         elif command.startswith("add"):
-            _, name, phone, *birthday = command.split()
-            birthday = birthday[0] if birthday else None
-            print(add_contact(name, phone, birthday))
+            _, name, *phones = command.split(", ")
+            print(add_contact(name, *phones))
         elif command.startswith("delete"):
-            _, name = command.split()
+            name = command.split()
             print(delete_contact(name))
         elif command.startswith("change"):
-            _, name, old_phone, new_phone = command.split()
-            print(change_phone(name, old_phone, new_phone))
+            _, name, _, new_phone = command.split(", ")
+            print(change_phone(name, new_phone))
         elif command.startswith("phone"):
-            _, name = command.split()
-            print(get_phone(name))
+            print(get_phones(command))
         elif command.startswith("show all"):
             print(show_all())
         elif command.startswith("show n records"):
-            _, n = command.split()
-            n = int(n)
+            n = int(command.split()[3])
             print(show_n_records(n))
+        elif command.startswith("days_to_birthday"):
+            print(days_to_birthday(command))
         elif command in ["good bye", "close", "exit"]:
             print("Good bye!")
             break
